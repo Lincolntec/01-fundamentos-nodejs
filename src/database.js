076f89e06f9ae6346b3b4f6616866d7ff110d5f0
Alcino José Lincoln de Souza <lincoln_tec@hotmail.com>
2023-04-05 16:12:20 -0300

    Finalizado o primeiro modulo de node.

--- src/database.js
+++ src/database.js
@@ -21,8 +21,21 @@ export class Database {
 
     }
 
-    select(table) {
-        const data = this.#database[table] ?? []
+    select(table, search) {
+        let data = this.#database[table] ?? []
+
+        if (search) {
+
+            data = data.filter(row => {
+                return Object.entries(search).some(([key, value]) => {
+                    return row[key].toLowerCase().includes(value.toLowerCase())
+
+
+                })
+            })
+
+        }
+
 
         return data
     }
@@ -38,4 +51,25 @@ export class Database {
 
         return data;
     }
+
+    update(table, id, data) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+
+            this.#database[table][rowIndex] = { id, ...data }
+            this.#persiste()
+        }
+    }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(row => row.id === id)
+
+        if (rowIndex > -1) {
+
+            this.#database[table].splice(rowIndex, 1)
+            this.#persiste()
+        }
+    }
+
 }
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,11 @@ export const routes = [
         method: 'GET',
         path: buildRoutePath('/users'),
         handler: (req, res) => {
-            const users = database.select('users')
+            const { search } = req.query
+            const users = database.select('users', search ? {
+                name: search,
+                email: search,
+            } : null)
 
             return res.end(JSON.stringify(users))
         }
@@ -29,11 +33,31 @@ export const routes = [
         }
     },
     {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+
+            const { name, email } = req.body
+
+            database.update('users', id, {
+                name,
+                email,
+
+            })
+
+            return res.writeHead(204).end()
+        },
+    },
+    {
         method: 'DELETE',
         path: buildRoutePath('/users/:id'),
         handler: (req, res) => {
-            console.log('Rota', req.params)
-            return res.end()
+            const { id } = req.params
+
+            database.delete('users', id)
+
+            return res.writeHead(204).end()
         },
     }
 ]
